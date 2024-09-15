c389f9b208739115307f13730fa60aea1759a4b1
abhirambbollina <abhibol@unc.edu>
2024-09-14 21:00:07 -0400

    Finished cq02 and ex02

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -6,12 +6,12 @@ __author__ = "730752527"
 def main_planner(guests: int) -> None:
     """Entrypoint of the program"""
 
-    # converts all function outputs and ints to strings to concatanate with the other strings
+    # Converts ints to strings to concatonate
     print("A Cozy Tea Party for " + str(guests) + " People!")
     print("Tea Bags: " + str(tea_bags(guests)))
     print("Treats: " + str(treats(guests)))
     # Uses tea_bags function and treats function to calcualate cost
-    print("Cost: " + str(cost(tea_bags(guests), treats(guests))))
+    print("Cost: $" + str(cost(tea_bags(guests), treats(guests))))
 
 
 def tea_bags(people: int) -> int:
@@ -21,12 +21,12 @@ def tea_bags(people: int) -> int:
 
 def treats(people: int) -> int:
     """calcualtes the number of treats needed for the party"""
-    return int(tea_bags(people) * 1.5)
+    return int(tea_bags(people=people) * 1.5)
 
 
 def cost(tea_count: int, treat_count: int) -> float:
     """colculates total cost of the party given numbver of tea bags and treats"""
-    return (tea_count * 0.50) * (treat_count * 0.75)
+    return (tea_count * 0.50) + (treat_count * 0.75)
 
 
 if __name__ == "__main__":
